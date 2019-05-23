402453a39ad43595de104a43c3b2662ce6e02975
killi18n <killi18n@gmail.com>
2019-05-23 11:55:47 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,23 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <Main/>
 </template>
 
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import Main from './routes/Main.vue';
+
+@Component({
+    components: {
+        Main: Main,
+    },
+})
+class App extends Vue {}
+
+export default App;
+</script>
+
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import '@/styles/index.scss';
 </style>
---
+++ src/routes/Main.vue
@@ -0,0 +1,4 @@
+<template>
+  <div>Main.vue</div>
+</template>
+
