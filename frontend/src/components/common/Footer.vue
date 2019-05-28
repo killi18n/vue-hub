32614d4fe552bbfbbffbd3350be68b58cea4f0bb
killi18n <killi18n@gmail.com>
2019-05-28 10:31:57 +0900

    commit

---
+++ frontend/src/components/common/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="footer">Footer</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+class Footer extends Vue {}
+
+export default Footer;
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/utils.scss';
+.footer {
+    height: 10rem;
+    background: $oc-gray-7;
+    color: white;
+
+    display: flex;
+    flex-direction: column;
+
+    align-items: center;
+    justify-content: center;
+}
+</style>
